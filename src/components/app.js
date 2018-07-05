6f638efeadab4365c68173e75c67dd685e034707
manel24 <manelcharouni@think-it.io>
2018-07-05 11:52:14 +0100

    mapped state weather data to props in weatherList container

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import SearchBar from "../containers/search_bar";
 import WeatherList from "../containers/weather_list";
+import weather_list from "../containers/weather_list";
 
 export default class App extends Component {
   render() {
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import connect from "react-redux";
+import { connect } from "react-redux";
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
   render() {
     return (
       <table className="table table-hover">
@@ -12,8 +20,8 @@ class WeatherList extends Component {
             <th>Pressure</th>
             <th>Humidity</th>
           </tr>
-          <tbody />
         </thead>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
@@ -21,10 +29,9 @@ class WeatherList extends Component {
 
 function mapStateToProps({ weather }) {
   // {weather} === state , then state.weather
-  return {
-    //weather: state.weather
-    wether: { weather } // {weather} ===
-  };
+
+  //weather: state.weather
+  return { weather }; // {weather} === {weather : weather}
 }
 
 export default connect(mapStateToProps)(WeatherList);
