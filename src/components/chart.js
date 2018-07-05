95067305bd1d3652bcb15e4cb884d38f432b3f2b
manel24 <manelcharouni@think-it.io>
2018-07-05 12:54:46 +0100

    labeling of units

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,20 @@
 import React, { COmponent } from "react";
-import { SparkLines, SparkLinesLine } from "react-sparklines";
+//import { SparkLines, SparkLinesLine, SparkLinesReferenceLine } from "react-sparklines";
 
+function average(data) {
+
+    return _.round(_.sum(data) / data.length)
+    
+}
 export default props => {
-  return (
-    <div>
-      <SparkLines height={120} width={180} data={props.data}>
-        <SparkLinesLine color={props.color} />
-      </SparkLines>
-    </div>
-  );
+  return undefined; //(
+
+  // <div>
+  //   <SparkLines height={120} width={180} data={props.data}>
+  //     <SparkLinesLine color={props.color} />
+  //     <SparkLinesReferenceLine type="avg" />
+  //   </SparkLines>
+  //   <div>{average(props.data)} {props.units} </div>;
+  // </div>
+  //);
 };
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,19 +6,33 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+
     console.log(temps);
     /*<SparkLines height={120} width={180} data={temps}>
             <SparkLinesLine color="red" />
         </SparkLines>
     */
-   //OR
-   //Using a reusable Chart COmponent:
-    /*<Chart data={temps} color="orange"/>; */
-
+    //OR
+    //Using a reusable Chart COmponent:
+    /*<Chart data={temps} color="orange" units="k"/>; 
+    <Chart data={pressures} color="green" units="hPa"/>; 
+    <Chart data={humidity} color="black" units="%"/>; 
+    
+    */
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>{temps[0]}</td>
+        <td>
+          {temps[0]} {"K"}
+        </td>
+        <td>
+          {pressures[0]} {"hPa"}
+        </td>
+        <td>
+          {humidity[0]} {"%"}
+        </td>
       </tr>
     );
   }
@@ -28,9 +42,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>city</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
