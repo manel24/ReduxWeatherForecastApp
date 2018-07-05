1bce76f88688d27d34700281e2181d5e7af6133c
manel24 <manelcharouni@think-it.io>
2018-07-05 10:54:44 +0100

    action received by the weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,17 @@
-export default function(state = null, action) {
-  console.log("action received", action);
+import { FETCH_WEATHER } from "../actions";
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      //should add the data array of weather data reated to the searched city to the state
+      //return [action.payload.data]; //we don't want to return the array of that city,
+      // we want to add it to the old array of other cities
+      //return state.push(action.payload.data) //this mutates the state !! NO
+      //return state.concat([action.payload.data]); //the right approach : concat doesn't mutate the old array,
+      return [action.payload.data, ...state]; // same line as thze above, but just
+    //nice ES6 syntax: returns [city,city, city]
+    //but crates a new array and add it to the state
+    // => returning a new version of our state
+  }
   return state;
 }
