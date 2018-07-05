410b57c9e248951796e7550bf86e69790987c8d7
manel24 <manelcharouni@think-it.io>
2018-07-05 12:16:30 +0100

    displaying city name and temps in the table: reactsparkleLines didnt work

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+//import { SparkLines, SparkLinesLine } from "react-sparklines";
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    console.log(temps);
+    /*<SparkLines height={120} width={180} data={temps}>
+            <SparkLinesLine color="red" />
+        </SparkLines>
+    */
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>{temps[0]}</td>
       </tr>
     );
   }
